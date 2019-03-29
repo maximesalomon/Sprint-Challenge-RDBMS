0acdc6f1fcefb6e6a8317aaf490df5f4c9377477
Maxime Salomon <maxime@croissant.io>
2019-03-29 10:27:28 +0100

    fixed actions seeds

--- data/seeds/02-actions.js
+++ data/seeds/02-actions.js
@@ -5,9 +5,9 @@ exports.seed = function(knex, Promise) {
     .then(function () {
       // Inserts seed entries
       return knex('actions').insert([
-        { name: 'action_1', description: "description_1 blah blah blah blah", notes: "note_1 this is a note.", completed: false, project_id: 1 },
-        { name: 'action_2', description: "description_2 blah blah blah blah", notes: "note_2 this is a note.", completed: false, project_id: 1 },
-        { name: 'action_3', description: "description_3 blah blah blah blah", notes: "note_3 this is a note.", completed: false, project_id: 2 },
+        { description: "description_1 blah blah blah blah", notes: "note_1 this is a note.", completed: false, project_id: 1 },
+        { description: "description_2 blah blah blah blah", notes: "note_2 this is a note.", completed: false, project_id: 1 },
+        { description: "description_3 blah blah blah blah", notes: "note_3 this is a note.", completed: false, project_id: 2 },
       ]);
     });
 };
